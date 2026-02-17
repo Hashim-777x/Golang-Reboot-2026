40a07b123caf26bd3b2670762e6fa6c6ad757b07
Hashim <113883060+Hashim-777x@users.noreply.github.com>
2026-02-17 23:56:16 +0530

    Update tcp_client.go

--- Phase-2/tcp_client.go
+++ Phase-2/tcp_client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"log/slog"
 	"net"
@@ -9,18 +10,42 @@ import (
 // peer
 
 type Peer struct {
-	conn net.Conn
+	conn  net.Conn
+	msgCh chan []byte
 }
 
-func NewPree(conn net.Conn) *Peer {
+func NewPree(conn net.Conn, msgCh chan []byte) *Peer {
 	return &Peer{
-		conn: conn,
+		conn:  conn,
+		msgCh: msgCh,
 	}
 }
 
+//	func (p *Peer) readLoop() {
+//		for {
+//			}
+//		}
 func (p *Peer) readLoop() {
-	for {
+	// 1. Create a buffer (a byte array slice)
+	// We allocate 2048 bytes (2KB) of memory to hold incoming data.
+	buf := make([]byte, 2048)
 
+	for {
+		// 2. Read from the connection into the buffer
+		// This is a "BLOCKING" call. The Goroutine pauses here
+		// until data arrives. It uses 0% CPU while waiting.
+		n, err := p.conn.Read(buf)
+		if err != nil {
+			return // Exit the loop and close the goroutine if the peer disconnects
+		}
+
+		// 3. Process the data
+		// We take a 'slice' of the buffer from index 0 to n (actual bytes read)
+		msgBuf := make([]byte, n)
+		copy(msgBuf, buf[:n])
+		p.msgCh <- msgBuf
+		// Log what we received
+		//slog.Info("received message", "msg", string(msg), "bytes", n)
 	}
 }
 
@@ -37,6 +62,7 @@ type Server struct {
 	ln        net.Listener
 	addPeerCh chan *Peer
 	quickCh   chan struct{}
+	msgCh     chan []byte
 }
 
 func NewServer(cfg Config) *Server {
@@ -48,6 +74,7 @@ func NewServer(cfg Config) *Server {
 		peer:      make(map[*Peer]bool),
 		addPeerCh: make(chan *Peer),
 		quickCh:   make(chan struct{}),
+		msgCh:     make(chan []byte),
 	}
 }
 
@@ -68,6 +95,9 @@ func (s *Server) Start() error {
 func (s *Server) loop() {
 	for {
 		select {
+		case rawMsg := <-s.msgCh:
+			fmt.Println(rawMsg)
+
 		case <-s.quickCh:
 			return
 		case peer := <-s.addPeerCh:
@@ -88,7 +118,7 @@ func (s *Server) acceptLoop() error {
 }
 
 func (s *Server) handleConn(conn net.Conn) {
-	peer := NewPree(conn)
+	peer := NewPree(conn, s.msgCh)
 	s.addPeerCh <- peer
 	slog.Info("new peer connected", "remoteAddr", conn.RemoteAddr())
 	peer.readLoop()
